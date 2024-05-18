d3a7cfb3f014f5014c890af22eedf0363b80525e
Shadman Rahman <shadmxn@gmail.com>
2024-05-17 22:30:51 -0400

    Updated with Template Literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed ✅: " + actual + " === " + expected);
+    console.log(`Assertion Passed ✅: ${actual} === ${expected}`);
   } else {
-    console.log("Assertion Failed 🛑: " + actual + " !== " + expected);
+    console.log(`Assertion Failed 🛑: ${actual} !== ${expected}`);
   }
 };
 assertEqual("Hamilton", "Hamilton");
