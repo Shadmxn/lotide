97cd5c103c2d20dd58a157e68464f83e693a6cc4
Shadman Rahman <shadmxn@gmail.com>
2024-05-20 16:22:41 -0400

    Test without.js

---
+++ without.js
@@ -0,0 +1,29 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+
+  if (eqArrays(arr1, arr2)) {
+
+    console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+
+    console.log(`🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const without = function(source, itemsToRemove)
+  return 
+
+
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
