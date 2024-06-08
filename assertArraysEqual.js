31b6c20c8da7c64d4ba381dc13ece12aa9e68e39
Shadman Rahman <shadmxn@gmail.com>
2024-06-07 20:59:52 -0400

    refractored assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed ✅: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed 🛑: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(arr1, arr2) {
 
@@ -29,22 +11,5 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-// Larry Ai Bot Suggested Calls
 
-// Should pass
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-
-// Should fail
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-
-// Testing with strings, should pass
-assertArraysEqual(["hello", "world"], ["hello", "world"]);
-
-// Testing with strings, should fail
-assertArraysEqual(["hello", "world"], ["hello", "bootcamp"]);
-
-// Edge case: empty arrays, should pass
-assertArraysEqual([], []);
-
-// Edge case: different lengths, should fail
-assertArraysEqual([1, 2], [1, 2, 3]);
+module.exports = assertArraysEqual;
