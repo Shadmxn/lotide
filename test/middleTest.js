a1f3d6c273d679952c21c55b12a6a2722d482e2f
Shadman Rahman <shadmxn@gmail.com>
2024-06-07 22:15:20 -0400

    mocha refractored

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,16 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 6, 7, 8, 4, 3]), [7, 8]);
-assertArraysEqual(middle([1, 6, 7, 8, 4]), [7]);
-assertArraysEqual(middle([1]), []);
+describe("middle", () => {
+  it("should return [7, 8] for [1, 6, 7, 8, 4, 3]", () => {
+    assert.deepEqual(middle([1, 6, 7, 8, 4, 3]), [7, 8]);
+  });
+
+  it("should return [7] for [1, 6, 7, 8, 4]", () => {
+    assert.deepEqual(middle([1, 6, 7, 8, 4]), [7]);
+  });
+
+  it("should return [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});
