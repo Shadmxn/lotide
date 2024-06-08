6104238899d7c1061edd8f27075100da88848ed8
Shadman Rahman <shadmxn@gmail.com>
2024-06-07 21:10:59 -0400

    added middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,6 @@
 const middle = require("../middle");
 const assertArraysEqual = require("../assertArraysEqual");
 
-console.log(middle([1,6,7,8,4,3]));
-console.log(middle([1,6,7,8,4]));
-console.log(middle([1]));
-
 assertArraysEqual(middle([1, 6, 7, 8, 4, 3]), [7, 8]);
 assertArraysEqual(middle([1, 6, 7, 8, 4]), [7]);
 assertArraysEqual(middle([1]), []);
